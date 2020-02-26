63160b8b46f3d9985b42f4ec9d28cb72f33bca9d
pulibhavana <bhavana.puli@thoughtworks.com>
2020-02-26 15:28:13 +0530

    Add call to isReverse

--- Main.js
+++ Main.js
@@ -1,20 +1,22 @@
 const functions = require('./Functions.js');
 function main()
 {
-    let list1 = [1,2,3,3,4];
-    let list2 = [3,4,5];
+    let list1 = [1,2,3,];
+    let list2 = [3,2,1];
 
-    result = functions.union(list1,list2);
-    console.log("The union of two lists "+result);
+    console.log("The union of two lists "+functions.union(list1,list2));
 
-    result = functions.intersection(list1,list2)
-    console.log("The intersection of two lists " +result);
+    console.log("The intersection of two lists " + functions.intersection(list1,list2));
 
-    result = functions.difference(list1,list2);
-    console.log("The differenece of two lists " + result);
+    console.log("The differenece of two lists " + functions.difference(list1,list2));
 
-    result = functions.issubset(list1,list2)
-    console.log(result);
+    console.log("The list2 is subset of list1 " +functions.isSubset(list1,list2));
+
+    console.log("The list2 is reverse of list1 "+ functions.isReverse(list1,list2));
+    
+
+
+
 
 }
 main()
