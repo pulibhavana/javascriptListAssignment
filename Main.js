7ed8edd4a1a6f509052ce095d479efa32fc18f7b
pulibhavana <bhavana.puli@thoughtworks.com>
2020-02-26 15:02:17 +0530

    Add call to difference

--- Main.js
+++ Main.js
@@ -1,15 +1,17 @@
 const functions = require('./Functions.js');
-const intersection = require('./Intersection.js');
 function main()
 {
     let list1 = [1,2,3,3,4];
     let list2 = [3,3,4,5,6];
 
     result = functions.union(list1,list2);
-    console.log(result);
+    console.log("The union of two lists "+result);
 
     result = functions.intersection(list1,list2)
-    console.log(result);
+    console.log("The intersection of two lists " +result);
+
+    result = functions.differenece(list1,list2);
+    console.log("The differenece of two lists " + result);
 
 }
 main()
