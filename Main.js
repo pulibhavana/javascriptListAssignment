b3442b2fe5a5f0dd91e43d46625b67fee4c7082a
pulibhavana <bhavana.puli@thoughtworks.com>
2020-02-26 12:53:57 +0530

    Modified the name of function

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ const functions = require('./Functions.js');
 function main()
 {
     let list1 = [1,2,3];
-    let list2 = [4,5,6];
+    let list2 = [3,5,6];
     result = functions.union(list1,list2);
     console.log(result);
 
