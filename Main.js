efab54f2321b5dcd5ffb0bfd5748778a4a840566
pulibhavana <bhavana.puli@thoughtworks.com>
2020-02-26 15:08:35 +0530

    Add call to issubset

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ const functions = require('./Functions.js');
 function main()
 {
     let list1 = [1,2,3,3,4];
-    let list2 = [3,3,4,5,6];
+    let list2 = [3,4,5];
 
     result = functions.union(list1,list2);
     console.log("The union of two lists "+result);
@@ -10,8 +10,11 @@ function main()
     result = functions.intersection(list1,list2)
     console.log("The intersection of two lists " +result);
 
-    result = functions.differenece(list1,list2);
+    result = functions.difference(list1,list2);
     console.log("The differenece of two lists " + result);
+
+    result = functions.issubset(list1,list2)
+    console.log(result);
 
 }
 main()
