dd6213c1aad3dd1f78b18f60c895a3c8e56655bc
pulibhavana <bhavana.puli@thoughtworks.com>
2020-02-26 14:44:51 +0530

    call to Intersection

--- Main.js
+++ Main.js
@@ -1,9 +1,14 @@
 const functions = require('./Functions.js');
+const intersection = require('./Intersection.js');
 function main()
 {
-    let list1 = [1,2,3];
-    let list2 = [3,5,6];
+    let list1 = [1,2,3,3,4];
+    let list2 = [3,3,4,5,6];
+
     result = functions.union(list1,list2);
+    console.log(result);
+
+    result = functions.intersection(list1,list2)
     console.log(result);
 
 }
